30e08f4ee432d2cc9a7831bd5b96b3c5828e8098
Danis Arslanov <arslanovdk@gmail.com>
2023-12-11 21:29:53 +0500

    test passed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const concatStrings = (a, b) => a + b;
 
+const isString = (value) => typeof value === 'string';
+
+const identifySign = (num) => num === 0 ? 'Ноль' : num > 0 ? 'Положительное число' : 'Отрицательное число';
+
+const reverseWords = (text) => text.split(' ').reverse().join(' ');
+
+const wordsCount = (text) => text === '' ? 0 : text.split(' ').length;
+
+export { concatStrings, isString, identifySign, reverseWords, wordsCount };
