3f73cb1c8c1c6839ebc9a4afed158bff17ca8d62
Danis Arslanov <arslanovdk@gmail.com>
2023-12-11 21:50:53 +0500

    fix

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const concatStrings = (a, b) => a + b;
 
 const isString = (value) => typeof value === 'string';
 
-const identifySign = (num) => num === 0 ? 'Ноль' : num > 0 ? 'Положительное число' : 'Отрицательное число';
+const identifySign = (num) => {
+  if (num === 0) {
+    return 'Ноль';
+  }
+  return num > 0 ? 'Положительное число' : 'Отрицательное число';
+};
 
 const reverseWords = (text) => text.split(' ').reverse().join(' ');
 
-const wordsCount = (text) => text === '' ? 0 : text.split(' ').length;
+const wordsCount = (text) => (text === '' ? 0 : text.split(' ').length);
 
-export { concatStrings, isString, identifySign, reverseWords, wordsCount };
+export {
+  concatStrings, isString, identifySign, reverseWords, wordsCount,
+};
